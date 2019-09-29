e5bea3ce0cc49e288ad4955da89ea3f668739ee6
Azhar Basit <azhar_basit@co-graph.co.jp>
2019-09-30 00:01:03 +0900

    CleanerServiceTest and ValidatePageServiceTest refactored to use WebSiteRepository Mock to run the tests.

--- crawler-service/src/main/java/com/topcoder/productsearch/cleaner/service/CleanerService.java
+++ crawler-service/src/main/java/com/topcoder/productsearch/cleaner/service/CleanerService.java
@@ -11,7 +11,6 @@ import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
