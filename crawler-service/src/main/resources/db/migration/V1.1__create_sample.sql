041e9caaf92042cda78b530fb66be1a8d305e4ad
Azhar Basit <azhar_basit@co-graph.co.jp>
2019-10-31 17:27:29 +0900

    Added some sample data in V1.1_create_sample.sql for web_site table

--- crawler-service/src/main/resources/db/migration/V1.1__create_sample.sql
+++ crawler-service/src/main/resources/db/migration/V1.1__create_sample.sql
@@ -6,16 +6,28 @@ INSERT INTO `web_sites`
 `description`,
 `url`,
 `content_url_patterns`,
+`supports_robots_txt`,
 `crawl_max_depth`,
-`crawl_time_limit`
+`crawl_time_limit`,
+`crawl_interval`,
+`parallel_size`,
+`timeout_page_download`,
+`retry_times`,
+`page_expired_period`
 ) VALUES (
 1,
 'UNIQLO',
 'Shop UNIQLO.com for the latest essentials for women, men, kids &amp; babies. Clothing with innovation and real value, engineered to enhance your life every day, all year round. UNIQLO US.',
 'https://www.uniqlo.com/us/en/',
 'https://www.uniqlo.com/us/en/[^/]+?.html.*?cgid=.*?$',
+1,
 10,
-3600
+3600,
+1000,
+12,
+2,
+2,
+30
 );
 
 
