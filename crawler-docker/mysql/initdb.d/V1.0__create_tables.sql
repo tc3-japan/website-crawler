b367efbdef615381e10fdac5031cbb3de68f2ee7
*twxqRBxtlr <kohata@tc3.co.jp>
2019-11-08 18:13:30 +0900

    add `deleted` column to web_sites table

--- crawler-docker/mysql/initdb.d/V1.0__create_tables.sql
+++ crawler-docker/mysql/initdb.d/V1.0__create_tables.sql
@@ -44,7 +44,7 @@ ENGINE = InnoDB
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_0900_ai_ci;
 
-CREATE TABLE `web_sites`  ( 
+CREATE TABLE `web_sites`  (
   `id`                    int(11) UNSIGNED AUTO_INCREMENT NOT NULL,
   `name`                  varchar(200) NOT NULL,
   `description`           varchar(1024) NULL,
@@ -52,16 +52,17 @@ CREATE TABLE `web_sites`  (
   `content_url_patterns`  varchar(2048) NULL,
   `created_at`            datetime NULL DEFAULT CURRENT_TIMESTAMP,
   `last_modified_at`      datetime NULL,
-  `supports_robots_txt` 	tinyint(1) UNSIGNED NULL DEFAULT 1,
-  `crawl_max_depth`       SMALLINT(5) UNSIGNED NULL DEFAULT 0,
-  `crawl_time_limit`      MEDIUMINT(8) UNSIGNED NULL DEFAULT 0,
-  `crawl_interval`        MEDIUMINT(8) UNISIGNED NULL DEFAULT 1000,
+  `supports_robots_txt`   tinyint(1) UNSIGNED NULL DEFAULT 1,
+  `crawl_max_depth`       smallint(5) UNSIGNED NULL DEFAULT 0,
+  `crawl_time_limit`      mediumint(8) UNSIGNED NULL DEFAULT 0,
+  `crawl_interval`        mediumint(8) UNSIGNED NULL DEFAULT 1000,
   `parallel_size`         tinyint(1) UNSIGNED NULL DEFAULT 12,
   `timeout_page_download` tinyint(1) UNSIGNED NULL DEFAULT 2,
   `retry_times`           tinyint(1) UNSIGNED NULL DEFAULT 2,
-  `page_expired_period`   SMALLINT(5) UNSIGNED NULL DEFAULT 30,
+  `page_expired_period`   smallint(5) UNSIGNED NULL DEFAULT 30,
   `category_extraction_pattern` varchar(2048) NULL,
   `content_selector`      varchar(2048) NULL,
+  `deleted`               tinyint(1) UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY(`id`)
 )
 ENGINE = InnoDB
