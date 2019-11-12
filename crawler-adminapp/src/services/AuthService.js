c386d7eccd5e04a24375e182745f83b16aac03e3
Thomas Couchman <tom_couchman@co-graph.com>
2019-11-12 13:11:54 +0900

    Fixes and cors update for service files, stopped tracking of config.json

--- crawler-adminapp/src/services/AuthService.js
+++ crawler-adminapp/src/services/AuthService.js
@@ -1,14 +1,14 @@
 import Api from './api';
 
 function logIn(username, password) {
-    return Api.get('/oauth/token', { username, password });
-};
+    return Api().post('/oauth/token', { username, password });
+}
 
 function logOut() {
-    return Api.get('/oauth/logout');
-};
+    return Api().get('/oauth/logout');
+}
 
 export default {
     logIn,
     logOut
-}
+};
