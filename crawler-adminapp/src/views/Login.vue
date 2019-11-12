e52127cc5a1f29b673aece48473f983fce284d94
Thomas Couchman <tom_couchman@co-graph.com>
2019-11-12 13:23:42 +0900

    Added site details modal component, imeplemented sites list and login functionality

--- crawler-adminapp/src/views/Login.vue
+++ crawler-adminapp/src/views/Login.vue
@@ -2,49 +2,52 @@
     <div>
         <div class="full-height bg-plum-plate bg-animation">
             <div class="d-flex full-height justify-content-center align-items-center">
-                <b-col md="8" class="mx-auto app-login-box">
+                <b-col md="10" class="mx-auto app-login-box">
                     <div class="modal-dialog w-100 mx-auto">
                         <div class="modal-content w-100">
-                            <div class="modal-body">
+                            <div class="modal-body p-3">
                                 <div class="h5 modal-title text-center">
                                     <h4 class="mt-2">
-                                        <div>Welcome back,</div>
+                                        <div class="mb-3">Log In</div>
                                         <span>Please sign in to your account below.</span>
                                     </h4>
                                 </div>
+                                <div class="pb-3">
+                                    <div class="divider"/>
+                                </div>
                                 <b-form-group id="exampleInputGroup1"
-                                              label-for="exampleInput1"
-                                              description="We'll never share your email with anyone else.">
-                                    <b-form-input id="exampleInput1"
-                                                  type="email"
-                                                  required
-                                                  placeholder="Enter email...">
-                                    </b-form-input>
+                                              label-for="exampleInput1">
+                                    <div class="position-relative row form-group">
+                                        <label class="col-sm-2 col-form-label">User ID</label>
+                                        <div class="col-sm-10">
+                                            <b-form-input size="lg" id="exampleInput1"
+                                                v-model="username"
+                                                type="text"
+                                                required
+                                                placeholder="Enter user ID...">
+                                            </b-form-input>
+                                        </div>
+                                    </div>
                                 </b-form-group>
+
                                 <b-form-group id="exampleInputGroup2"
                                               label-for="exampleInput2">
-                                    <b-form-input id="exampleInput2"
-                                                  type="password"
-                                                  required
-                                                  placeholder="Enter password...">
-                                    </b-form-input>
+                                    <div class="position-relative row form-group">
+                                        <label class="col-sm-2 col-form-label">Password</label>
+                                        <div class="col-sm-10">
+                                            <b-form-input size="lg" id="exampleInput2"
+                                                v-model="password"
+                                                type="password"
+                                                required
+                                                placeholder="Enter password...">
+                                            </b-form-input>
+                                        </div>
+                                    </div>
                                 </b-form-group>
-                                <b-form-checkbox name="check" id="exampleCheck">
-                                    Keep me logged in
-                                </b-form-checkbox>
-                                <div class="divider"/>
-                                <h6 class="mb-0">
-                                    No account?
-                                    <a href="javascript:void(0);" class="text-primary">Sign up now</a>
-                                </h6>
                             </div>
                             <div class="modal-footer clearfix">
-                                <div class="float-left">
-                                    <a href="javascript:void(0);" class="btn-lg btn btn-link">Recover
-                                        Password</a>
-                                </div>
-                                <div class="float-right">
-                                    <b-button variant="primary" size="lg">Login to Dashboard</b-button>
+                                <div class="mx-auto">
+                                    <b-button @click="logIn" variant="primary" size="lg">Login</b-button>
                                 </div>
                             </div>
                         </div>
@@ -57,6 +60,32 @@
         </div>
     </div>
 </template>
+
+<script>
+import AuthService from '@/services/AuthService';
+
+export default {
+    name: 'Login',
+    data() {
+        return {
+            username : '',
+            password : ''
+        };
+    },
+    methods: {
+        logIn() {
+            AuthService.logIn(this.username, this.password)
+            .then(data => {
+                console.log('token', data.data.token);
+                this.$router.push('/home');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+     }
+};
+</script>
 
 <style>
 
