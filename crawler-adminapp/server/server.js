391488668c439bc653c884e3e039ad4dfa67c472
Thomas Couchman <tom_couchman@co-graph.com>
2019-11-20 15:49:17 +0900

    Removed unneeded console output

--- crawler-adminapp/server/server.js
+++ crawler-adminapp/server/server.js
@@ -10,7 +10,6 @@ app.get('*', function(req, res) {
     res.sendFile('index.html', { root: path.join(__dirname, '../dist/') });
 });
 
-console.log('p',config.client.port);
 var port = process.env.PORT || config.client.port;
 
 app.listen(port, () => {
