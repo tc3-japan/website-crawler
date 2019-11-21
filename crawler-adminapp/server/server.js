a48a51d36997c6fe5196fa15f4653ad853d71a31
Thomas Couchman <tom_couchman@co-graph.com>
2019-11-21 17:27:32 +0900

    .env files are now used in place of the config file for Vue site settings, server.js will look at PORT environment value and will fallback to 'port' value in /server/config.json if present

--- crawler-adminapp/server/server.js
+++ crawler-adminapp/server/server.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const path = require('path');
-const config = require('../config');
+
+let config = null;
+try {
+    config = require('./config');
+}
+catch(e) {
+
+}
 
 app = express();
 
+//  Sever VUE dist files
 app.use(express.static(path.join(__dirname, '../dist/')))
 
+// For all other requests serve the index file
 app.get('*', function(req, res) {
     res.sendFile('index.html', { root: path.join(__dirname, '../dist/') });
 });
 
-var port = process.env.PORT || config.client.port;
+var port = process.env.PORT || (config ? config.port : '');
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+if (port) {
+    app.listen(port, () => {
+        console.log(`Server is listening on port ${port}`);
+    });
+}
+else {
+    console.log('Cannot start server: No port value has been set');
+}
