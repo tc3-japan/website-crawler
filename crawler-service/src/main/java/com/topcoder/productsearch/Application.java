465bfa5ef69e198a52686fb680234ada2c135243
*twxqRBxtlr <kohata@tc3.co.jp>
2019-12-03 10:06:17 +0900

    Add LocaleResolver and Japanese resources

--- crawler-service/src/main/java/com/topcoder/productsearch/Application.java
+++ crawler-service/src/main/java/com/topcoder/productsearch/Application.java
@@ -1,6 +1,7 @@
 package com.topcoder.productsearch;
 
-import com.topcoder.productsearch.api.securities.RestCondition;
+import java.util.Locale;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
@@ -10,6 +11,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import com.topcoder.productsearch.api.securities.RestCondition;
 
 @SpringBootApplication(exclude = {FlywayAutoConfiguration.class, SolrAutoConfiguration.class})
 @ComponentScan({
@@ -27,7 +32,7 @@ public class Application {
    * @throws Exception if any error occurs
    */
   public static void main(String[] args) {
-    
+
     boolean isRestMode = false;
     for (String arg : args) {
       if ("--rest".equalsIgnoreCase(arg)) {
@@ -43,6 +48,7 @@ public class Application {
       ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
       messageSource.setBasename("classpath:messages");
       messageSource.setDefaultEncoding("UTF-8");
+      messageSource.setCacheMillis(100);
       return messageSource;
   }
 
@@ -51,5 +57,12 @@ public class Application {
       LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
       bean.setValidationMessageSource(messageSource());
       return bean;
+  }
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    AcceptHeaderLocaleResolver lr = new AcceptHeaderLocaleResolver();
+    lr.setDefaultLocale(Locale.US);
+    return lr;
   }
 }
