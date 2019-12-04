5df20dd45f53bdb2346f3565d60b3604b91684d4
Azhar Basit <azhar_basit@co-graph.co.jp>
2019-12-04 15:18:36 +0900

    WebsiteService.java adding webSite.setLastModifed on update.

--- crawler-adminapp/vue.config.js
+++ crawler-adminapp/vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     devServer: {
-        proxy: 'http://ae91f8bcb014711ea9701063bddf76ed-904913025.ap-northeast-1.elb.amazonaws.com',
+	// proxy: 'http://ae91f8bcb014711ea9701063bddf76ed-904913025.ap-northeast-1.elb.amazonaws.com',
+	proxy: 'http://localhost:8090',
         disableHostCheck: true
     }
 };
