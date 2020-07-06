c5f13e947f547110583c0daadd8f71a9634eb3ad
ykohata <kohata@tc3.co.jp>
2020-07-06 13:00:15 +0900

    Improvement and bug fixes on GenTruthService

--- crawler-service/src/main/java/com/topcoder/productsearch/common/repository/SourceURLRepository.java
+++ crawler-service/src/main/java/com/topcoder/productsearch/common/repository/SourceURLRepository.java
@@ -1,13 +1,16 @@
 package com.topcoder.productsearch.common.repository;
 
-import com.topcoder.productsearch.common.entity.SourceURL;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import com.topcoder.productsearch.common.entity.SourceURL;
 
 /**
  * The repository defines operations on SourceURL entity.
  */
 @Repository
 public interface SourceURLRepository extends CrudRepository<SourceURL, Integer> {
-  SourceURL findByUrlAndPageId(String url, Integer pageId);
+  List<SourceURL> findByUrlAndPageId(String url, Integer pageId);
 }
